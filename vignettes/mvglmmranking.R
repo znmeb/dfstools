6d500d313445fb473e7d36b935449dbc60e64168
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-01-13 03:01:51 -0800

    Make the copy-and-paste a code chunk

--- vignettes/mvglmmranking.R
+++ vignettes/mvglmmranking.R
@@ -4,3 +4,11 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
+## ----eval=FALSE----------------------------------------------------------
+#  library(tidysportsfeeds)
+#  stattleshipR::set_token(token)
+#  nba_games <- get_games("nba")
+#  nba_result <- project_upcoming_games(nba_games)
+#  nba_projections <- nba_result$projections
+#  readr::write_excel_csv(nba_projections, "nba_projections.csv")
+
