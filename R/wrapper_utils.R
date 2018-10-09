660c56a1e971d3db740d0f0c1c5a0d04cecd2ef3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-10-08 20:29:19 -0700

    msf_past_nba_games without MySportsFeeds wrapper!

--- R/wrapper_utils.R
+++ R/wrapper_utils.R
@@ -1,25 +1,30 @@
 #' @title MySportsFeeds Past NBA Games
 #' @name msf_past_nba_games
-#' @description Returns a data frame of past NBA games from MySportsFeeds
+#' @description Returns a data frame of past NBA games from MySportsFeeds version 2.0 API
 #' @importFrom dplyr bind_rows
 #' @importFrom dplyr %>%
 #' @importFrom dplyr mutate
+#' @importFrom dplyr arrange
 #' @importFrom tibble tibble
-#' @importFrom mysportsfeedsR msf_get_results
+#' @importFrom httr GET
+#' @importFrom httr authenticate
+#' @importFrom httr status_code
+#' @importFrom jsonlite fromJSON
 #' @importFrom lubridate with_tz
 #' @importFrom lubridate as_datetime
+#' @importFrom lubridate as_date
 #' @export msf_past_nba_games
 #' @param seasons vector of season codes
+#' @param apikey your MySportsFeeds API key (version 2.0!)
 #' @return a tibble of completed NBA games
 #' @details `msf_past_nba_games` adds two columns at the right of the tibble:
-#'   `season`, the source season of the row, and `startEastern`, the game
-#'   start time in the Eastern USA timezone ("EST5EDT").
+#'   `season`, the source season of the row, and `date`, the game
+#'   date (started) in the Eastern USA timezone ("EST5EDT"). The returned
+#'   tibble will be sorted in chronological order.
 #' @examples
 #' \dontrun{
 #' apikey <- "your_API key"
 #' library(dfstools)
-#' library(mysportsfeedsR)
-#' authenticate_v2_x(apikey)
 #' seasons <- c(
 #'   "2018-playoff",
 #'   "2017-2018-regular",
@@ -27,31 +32,40 @@
 #'   "2016-2017-regular",
 #'   "2016-playoff",
 #'   "2015-2016-regular")
-#' nba_games <- msf_past_nba_games(seasons)
+#' nba_games <- msf_past_nba_games(seasons, apikey)
 #' }
 
-msf_past_nba_games <- function(seasons) {
+msf_past_nba_games <- function(seasons, apikey) {
   games <- tibble::tibble()
   for (ixseason in seasons) {
-    result <- mysportsfeedsR::msf_get_results(
-      version = "2.0",
-      league = "nba",
-      season = ixseason,
-      feed = "seasonal_games",
-      verbose = TRUE)
-    game_list <- result[["api_json"]][["games"]] %>%
+    ixurl <- paste(
+      "https://api.mysportsfeeds.com/v2.0/pull/nba",
+      ixseason,
+      "games.json",
+      sep = "/"
+    )
+    response <- httr::GET(
+      url = ixurl,
+      httr::authenticate(apikey, "MYSPORTSFEEDS")
+    )
+    status_code = httr::status_code(response)
+    if (status_code != 200) {
+      print(response)
+      stop(paste("status_code =", status_code))
+    }
+    json <- response %>% httr::content(as = "text")
+    game_list <- jsonlite::fromJSON(json, flatten = TRUE)[["games"]] %>%
       tibble::as_tibble() %>%
       dplyr::mutate(
         season = ixseason,
-        startEastern = lubridate::with_tz(
-          lubridate::as_datetime(schedule.startTime),
-          "EST5EDT"
-        )
+        date = lubridate::as_datetime(schedule.startTime) %>%
+          lubridate::with_tz("EST5EDT") %>%
+          lubridate::as_date() %>%
+          as.character()
       )
     games <- dplyr::bind_rows(games, game_list)
   }
-
-  return(games)
+  return(games %>% dplyr::arrange(schedule.startTime))
 }
 
 #' @title MySportsFeeds Past NBA DFS
