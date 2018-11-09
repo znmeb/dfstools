1ca1be6ca774036df82ef2c5316b3464a6a4929e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-11-09 01:16:22 -0800

    script to collect column names

---
+++ inst/notebooks/collect_column_names.R
@@ -0,0 +1,60 @@
+library(dplyr)
+library(dfstools)
+library(snakecase)
+library(tibble)
+
+column_name_table <- tibble()
+for (league in c("nba", "nhl", "nfl", "mlb")) {
+  table <- "games"
+  print(paste(league, table))
+  games <- msf_seasonal_games(
+    season, league, verbose = TRUE)$games
+  column_names <- games %>%
+    colnames() %>% to_snake_case()
+  rows <- tibble(league, season, table, column_names)
+  column_name_table <- column_name_table %>%
+    bind_rows(rows)
+
+  teams <- games %>%
+    dplyr::select(team = schedule_home_team_abbreviation) %>%
+    dplyr::distinct()
+  team <- teams[1]$team
+  print(team)
+
+  table <- "players"
+  print(paste(league, table))
+  column_names <- msf_seasonal_players(
+    season, league, verbose = TRUE)$players %>%
+    colnames() %>% to_snake_case()
+  rows <- tibble(league, season, table, column_names)
+  column_name_table <- column_name_table %>%
+    bind_rows(rows)
+
+  table <- "dfs"
+  print(paste(league, table))
+  column_names <- msf_seasonal_dfs(
+    season, league, team, verbose = TRUE) %>%
+    colnames() %>% to_snake_case()
+  rows <- tibble(league, season, table, column_names)
+  column_name_table <- column_name_table %>%
+    bind_rows(rows)
+
+  table <- "player_gamelogs"
+  print(paste(league, table))
+  column_names <- msf_seasonal_player_gamelogs(
+    season, league, team, verbose = TRUE)$player_gamelogs %>%
+    colnames() %>% to_snake_case()
+  rows <- tibble(league, season, table, column_names)
+  column_name_table <- column_name_table %>%
+    bind_rows(rows)
+
+  table <- "team_gamelogs"
+  print(paste(league, table))
+  column_names <- msf_seasonal_team_gamelogs(
+    season, league, team, verbose = TRUE)$team_gamelogs %>%
+    colnames() %>% to_snake_case()
+  rows <- tibble(league, season, table, column_names)
+  column_name_table <- column_name_table %>%
+    bind_rows(rows)
+
+}
