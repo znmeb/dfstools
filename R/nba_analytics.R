66222df3a326082646c3408d3f2c570cc1184e3e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-12-27 18:48:16 -0800

    NBA season totals fetch and clean

---
+++ R/nba_analytics.R
@@ -0,0 +1,77 @@
+#  Text feet and inches to centimeters
+.feet_inches_to_cm <- function(height) {
+  feet <- as.numeric(sub("\'.*$", "", height))
+  inches <- sub("^.*\'", "", height)
+  inches <- as.numeric(sub("\".*$", "", inches))
+  return(2.54 * (12.0 * feet + inches))
+}
+
+#' @title NBA Player Season Totals
+#' @name nba_player_season_totals
+#' @description fetches the player season totals for base box score statistics
+#' @importFrom dplyr %>%
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select_at
+#' @export nba_player_season_totals
+#' @param season a valid MySportsFeeds v2.0 API season name
+#' @return a tibble of tidied basic box scores
+#' @examples
+#' \dontrun{
+#' player_totals <- dfstools::nba_player_season_totals("current")
+#' }
+
+nba_player_season_totals <- function(season) {
+  response <- msf_seasonal_player_stats_totals(
+    league = "nba",
+    season = season,
+    verbose = FALSE
+  )
+  raw_data <- response[["player_stats_totals"]] %>%
+    dplyr::filter(player_current_roster_status == "ROSTER") %>%
+    dplyr::mutate(
+      player_name = paste(player_first_name, player_last_name),
+      player_height_cm = .feet_inches_to_cm(player_height),
+      stats_minutes_played = stats_miscellaneous_min_seconds / 60.0
+    )
+  use_columns <- c(
+    "player_name",
+    "player_primary_position",
+    "team_abbreviation",
+    "player_height",
+    "player_height_cm",
+    "player_weight",
+    "player_birth_date",
+    "player_age",
+    "player_rookie",
+    "stats_minutes_played",
+    "stats_games_played",
+    "stats_miscellaneous_games_started",
+    "stats_field_goals_fg_2_pt_att",
+    "stats_field_goals_fg_2_pt_made",
+    "stats_field_goals_fg_3_pt_att",
+    "stats_field_goals_fg_3_pt_made",
+    "stats_field_goals_fg_att",
+    "stats_field_goals_fg_made",
+    "stats_free_throws_ft_att",
+    "stats_free_throws_ft_made",
+    "stats_rebounds_off_reb",
+    "stats_rebounds_def_reb",
+    "stats_rebounds_reb",
+    "stats_offense_ast",
+    "stats_offense_pts",
+    "stats_defense_tov",
+    "stats_defense_stl",
+    "stats_defense_blk",
+    "stats_miscellaneous_fouls"
+  )
+  return(raw_data %>% dplyr::select_at(.vars = use_columns))
+}
+
+utils::globalVariables(c(
+  "player_current_roster_status",
+  "player_first_name",
+  "player_height",
+  "player_last_name",
+  "stats_miscellaneous_min_seconds"
+))
