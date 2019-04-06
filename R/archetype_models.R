7f7214db4853040b175e6f245b6d668c59dbbf9a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-04-05 19:30:50 -0700

    partial refactor of the archetypes code

--- R/archetype_models.R
+++ R/archetype_models.R
@@ -171,6 +171,160 @@ nba_archetype_search <- function(player_totals, num_steps = 7) {
     archetype_models = archetype_models))
 }
 
+#' @title Archetype Search
+#' @name archetype_search
+#' @description stepwise search of archetype counts
+#' @importFrom archetypes stepArchetypes
+#' @importFrom archetypes bestModel
+#' @importFrom archetypes robustArchetypes
+#' @importFrom dplyr %>%
+#' @importFrom dplyr select
+#' @importFrom tibble column_to_rownames
+#' @importFrom tibble rownames_to_column
+#' @importFrom tibble as_tibble
+#' @export archetype_search
+#' @param player_totals a tibble of the values to use for archetypal analysis.
+#' The first column must be the player name, which must match the player name
+#' in the player_labels. The archetypes will be sorted on the second columns.
+#' @param player_labels a tibble with the labels (player and team names,
+#' positions, etc.). They may overlap with the totals. The leftmost column must
+#' be the player name, which must match the player name in `player_totals`..
+#' @param num_steps number of steps to use (default 1:5)
+#' @param nrep number of repetitions at each step (default 4)
+#' @param verbose should the search be verbose? (default FALSE)
+#' @return a list of
+#' \itemize{
+#' \item archetype_parameters the parameters that define each archetype
+#' \item player_alphas the players tagged with their loadings on each archetype
+#' \item archetype_model the model object - the `bestModel` with `num_steps`
+#' archetypes
+#' \item all of the models}
+
+archetype_search <- function(player_totals, player_labels,
+                             num_steps = 1:5, nrep = 4, verbose = FALSE) {
+
+  input_matrix <- player_totals %>%
+    tibble::column_to_rownames(var = "player_name") %>%
+    as.matrix()
+  set.seed(1776)
+  archetype_models <- archetypes::stepArchetypes(
+    data = input_matrix,
+    k = num_steps,
+    nrep = nrep,
+    method = robustArchetypes,
+    verbose = verbose
+  )
+  archetype_model <- archetypes::bestModel(archetype_models[max(num_steps)])
+
+  # get the archetype_parameters
+  archetype_parameters <- t(archetypes::parameters(archetype_model))
+
+  # compute the ordering
+  ordering <- order(-archetype_parameters[1, ])
+
+  # get the player_alphas
+  player_alphas <- archetype_model[["alphas"]]
+  rownames(player_alphas) <- rownames(input_matrix)
+
+  # reorder the columns
+  archetype_parameters <- archetype_parameters[, ordering]
+  player_alphas <- player_alphas[, ordering]
+
+  # use archetypal players for column names
+  name_vector <- c()
+  for (i in 1:ncol(player_alphas)) {
+    name_vector <- c(name_vector, names(which.max(player_alphas[, i])))
+  }
+  name_vector[ncol(player_alphas)] <- "Bench"
+  colnames(archetype_parameters) <- name_vector
+  colnames(player_alphas) <- name_vector
+
+  # make tibbles
+  player_alphas <- player_alphas %>% as.data.frame() %>%
+    tibble::rownames_to_column(var = "player_name") %>% as_tibble()
+  player_alphas <- dplyr::left_join(player_labels, player_alphas)
+  archetype_parameters <- archetype_parameters %>% as.data.frame() %>%
+    tibble::rownames_to_column(var = "statistic") %>% as_tibble()
+
+  return(list(
+    archetype_parameters = archetype_parameters,
+    player_alphas = player_alphas,
+    archetype_model = archetype_model,
+    archetype_models = archetype_models))
+}
+
+#' @title Archetypal Analysis
+#' @name compute_archetypes
+#' @description perform an "archetypal athletes" analysis
+#' @importFrom archetypes archetypes
+#' @importFrom dplyr %>%
+#' @importFrom dplyr select
+#' @importFrom tibble column_to_rownames
+#' @importFrom tibble rownames_to_column
+#' @importFrom tibble as_tibble
+#' @export compute_archetypes
+#' @param player_totals a tibble of the values to use for archetypal analysis.
+#' The first column must be the player name, which must match the player name
+#' in the player_labels. The archetypes will be sorted on the second columns.
+#' @param player_labels a tibble with the labels (player and team names,
+#' positions, etc.). They may overlap with the totals. The leftmost column must
+#' be the player name, which must match the player name in `player_totals`..
+#' @param num_archetypes number of archetypes to use (default 3)
+#' @return a list of
+#' \itemize{
+#' \item archetype_parameters the parameters that define each archetype
+#' \item player_alphas the players tagged with their loadings on each archetype
+#' \item archetype_model the model object}
+
+compute_archetypes <- function(player_totals, player_labels,
+                               num_archetypes = 3) {
+
+  input_matrix <- player_totals %>%
+    tibble::column_to_rownames(var = "player_name") %>%
+    as.matrix()
+  set.seed(1776)
+  archetype_model <- archetypes(
+    data = input_matrix,
+    k = num_archetypes,
+    verbose = FALSE
+  )
+
+  # get the archetype_parameters
+  archetype_parameters <- t(archetypes::parameters(archetype_model))
+
+  # compute the ordering
+  ordering <- order(-archetype_parameters[1, ])
+
+  # get the player_alphas
+  player_alphas <- archetype_model[["alphas"]]
+  rownames(player_alphas) <- rownames(input_matrix)
+
+  # reorder the columns
+  archetype_parameters <- archetype_parameters[, ordering]
+  player_alphas <- player_alphas[, ordering]
+
+  # use archetypal players for column names
+  name_vector <- c()
+  for (i in 1:ncol(player_alphas)) {
+    name_vector <- c(name_vector, names(which.max(player_alphas[, i])))
+  }
+  name_vector[ncol(player_alphas)] <- "Bench"
+  colnames(archetype_parameters) <- name_vector
+  colnames(player_alphas) <- name_vector
+
+  # make tibbles
+  player_alphas <- player_alphas %>% as.data.frame() %>%
+    tibble::rownames_to_column(var = "player_name") %>% as_tibble()
+  player_alphas <- dplyr::left_join(player_labels, player_alphas)
+  archetype_parameters <- archetype_parameters %>% as.data.frame() %>%
+    tibble::rownames_to_column(var = "statistic") %>% as_tibble()
+
+  return(list(
+    archetype_parameters = archetype_parameters,
+    player_alphas = player_alphas,
+    archetype_model = archetype_model))
+}
+
 utils::globalVariables(c(
   "player_current_team_abbreviation",
   "player_height_ft",
