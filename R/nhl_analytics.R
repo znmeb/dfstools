04db28c974eb7d367d1999b0599b97f9123424f3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-10-08 12:00:32 -0700

    more code

--- R/nhl_analytics.R
+++ R/nhl_analytics.R
@@ -5,7 +5,9 @@
   inches <- as.numeric(sub("\".*$", "", inches))
   return(feet + inches / 12.0)
 }
-
+library(dfstools)
+library(dplyr)
+source("~/.ssh/msf_key")
 nhl_raw <- dfstools::msf_seasonal_player_stats_totals("nhl", "2018-2019-regular") %>%
   dplyr::filter(player_current_roster_status == "ROSTER") %>%
   dplyr::mutate(
@@ -18,4 +20,54 @@ goalies <- nhl_raw %>% dplyr::filter(player_primary_position == "G") %>%
 skaters <- nhl_raw %>% dplyr::filter(player_primary_position != "G") %>%
   dplyr::mutate(stats_minutes_played = stats_shifts_time_on_ice_seconds / 60.0) %>%
   dplyr::arrange(desc(stats_minutes_played))
+label_columns <- c(
+  "player_name",
+  "player_primary_position",
+  "player_current_team_abbreviation",
+  "player_height",
+  "player_height_ft",
+  "player_weight",
+  "player_birth_date",
+  "player_age",
+  "player_rookie"
+)
+labels <- nhl_raw %>%
+  dplyr::select(label_columns) %>%
+  dplyr::arrange(player_name) %>%
+  unique()
+
+goalie_stats_columns <- c(
+  "stats_minutes_played",
+  "stats_games_played",
+  grep("_goaltending_", names(goalies), value = TRUE) %>%
+    grep("percent", ., value = TRUE, invert = TRUE)
+)
+
+skater_stats_columns <- c(
+  "stats_minutes_played",
+  "stats_games_played",
+  grep("_scoring_", names(goalies), value = TRUE) %>%
+    grep("percent", ., value = TRUE, invert = TRUE),
+  grep("_skating_", names(goalies), value = TRUE) %>%
+    grep("percent", ., value = TRUE, invert = TRUE),
+  grep("_shifts_", names(goalies), value = TRUE)
+)
+
+goalie_totals <- goalies %>%
+  dplyr::group_by(player_name) %>%
+  dplyr::summarize_at(
+    .vars = dplyr::vars(goalie_stats_columns),
+    .funs = dplyr::funs(sum)
+  ) %>%
+  dplyr::arrange(player_name) %>%
+  dplyr::ungroup()
+
+skater_totals <- skaters %>%
+  dplyr::group_by(player_name) %>%
+  dplyr::summarize_at(
+    .vars = dplyr::vars(skater_stats_columns),
+    .funs = dplyr::funs(sum)
+  ) %>%
+  dplyr::arrange(player_name) %>%
+  dplyr::ungroup()
 
