006309e7bbf51cd67a330a1368029a6b989c4f96
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-08-08 22:48:57 -0700

    BigDataBall input functions

--- R/wnba.R
+++ R/wnba.R
@@ -1,3 +1,66 @@
+#' @title WNBA BigDataBall DFS
+#' @name wnba_bdb_dfs
+#' @description fetches a BigDataBall WNBA player box score
+#' @export wnba_bdb_dfs
+#' @importFrom readxl read_excel
+#' @param bdb_excel_file a BigDataBall WNBA DFS file
+#' @return a tibble with the DFS data
+#' @examples
+#' \dontrun{
+#' wnba_dfs <- dfstools::wnba_bdb_dfs(
+#'   "~/Dropbox/20-wnba-dfs/08-07-2020-wnba-season-dfs-feed.xlsx"
+#' )
+#' View(wnba_dfs)
+#' }
+
+wnba_bdb_dfs <- function(bdb_excel_file) {
+  df <- readxl::read_excel(
+    bdb_excel_file,
+    col_names = FALSE,
+    skip = 3
+  )
+  names(df) <- c(
+    "dataset",
+    "date",
+    "player_full_name",
+    "own_team",
+    "opp_team",
+    "starter",
+    "venue_road_home",
+    "min",
+    "usage_rate_percent",
+    "draftkings_position",
+    "fanduel_position",
+    "draftkings_salary",
+    "fanduel_salary",
+    "draftkings_points",
+    "fanduel_points"
+  )
+  return(df)
+}
+
+#' @title WNBA BigDataBall Player Box Score
+#' @name wnba_bdb_player_box_score
+#' @description fetches a BigDataBall WNBA player box score
+#' @export wnba_bdb_player_box_score
+#' @importFrom dplyr %>%
+#' @importFrom janitor clean_names
+#' @importFrom readxl read_excel
+#' @param bdb_excel_file a BigDataBall WNBA player box score file
+#' @return a tibble with the player box score
+#' @examples
+#' \dontrun{
+#' wnba_player_box_score <- dfstools::wnba_bdb_player_box_score(
+#'   "~/Dropbox/20-wnba-player/08-07-2020-wnba-season-player-feed.xlsx"
+#' )
+#' View(wnba_player_box_score)
+#' }
+
+wnba_bdb_player_box_score <- function(bdb_excel_file) {
+  readxl::read_excel(bdb_excel_file) %>%
+    janitor::clean_names()
+}
+
 #' @title Basketball Reference WNBA Schedule
 #' @name wnba_2020_schedule_bbref
 #' @description fetches the 2020 WNBA schedule from Basketball-reference.com
@@ -21,15 +84,15 @@
 
 wnba_2020_schedule_bbref <- function() {
   df <- xml2::read_html(
-      "https://www.basketball-reference.com/wnba/years/2020-schedule.html"
-    ) %>% rvest::html_table()
+    "https://www.basketball-reference.com/wnba/years/2020-schedule.html"
+  ) %>% rvest::html_table()
   df[[1]] %>% dplyr::select(
     raw_date = "Date",
     away = "Visitor/Neutral",
     home = "Home/Neutral") %>%
-  dplyr::mutate(
-    date = lubridate::mdy(raw_date),
-  )
+    dplyr::mutate(
+      date = lubridate::mdy(raw_date),
+    )
 }
 
 #' @title Rest days
